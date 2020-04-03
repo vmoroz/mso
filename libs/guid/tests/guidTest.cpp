4d8b7aefb225cadb13aa1c9141d2071910d91391
Vladimir Morozov <vmorozov@microsoft.com>
2020-04-03 01:01:01 -0700

    formatted code

--- libs/guid/tests/guidTest.cpp
+++ libs/guid/tests/guidTest.cpp
@@ -5,70 +5,84 @@
 #include "motifCpp/testCheck.h"
 
 MSO_STRUCT_GUID(IBaseInterface, "38E23DC7-92B1-4B21-B5FE-6EA786817915")
-struct DECLSPEC_NOVTABLE IBaseInterface {
+struct DECLSPEC_NOVTABLE IBaseInterface
+{
   virtual void BaseMethod() = 0;
 };
 
 MSO_STRUCT_GUID(IDerivedInterface, "9E11BC42-1416-45B1-9712-AA4AAD87F78B")
-struct DECLSPEC_NOVTABLE IDerivedInterface : public IBaseInterface {
+struct DECLSPEC_NOVTABLE IDerivedInterface : public IBaseInterface
+{
   virtual void DerivedMethod() = 0;
 };
 
 MSO_CLASS_GUID(MyClass, "866E3389-0194-4416-94CE-6BE8A5185387")
-class MyClass {
+class MyClass
+{
   virtual void DerivedMethod() = 0;
 };
 
 MSO_STRUCT_GUID(AlreadyDefinedType1, "BAA524C8-F5D1-4CE7-AE24-1FF5EAB306B2")
-struct AlreadyDefinedType1 {
+struct AlreadyDefinedType1
+{
   virtual void Method() = 0;
 };
 
 MSO_CLASS_GUID(AlreadyDefinedType2, "8C76ECFE-0715-4DA1-9746-2FD4DE39118F")
-class AlreadyDefinedType2 {
+class AlreadyDefinedType2
+{
   virtual void Method() = 0;
 };
 
-class TypeWithoutGuid {};
+class TypeWithoutGuid
+{
+};
 
 // Associate GUIDs in a namespace
-namespace MyAppTest {
-namespace MyDetails {
+namespace MyAppTest { namespace MyDetails {
 
 MSO_STRUCT_GUID(IBaseInterface2, "86DE7178-00E6-4A40-8B2C-BA2FBFFB4D06")
-struct DECLSPEC_NOVTABLE IBaseInterface2 {
+struct DECLSPEC_NOVTABLE IBaseInterface2
+{
   virtual void BaseMethod() = 0;
 };
 
 MSO_STRUCT_GUID(IDerivedInterface2, "C91CAD11-98AB-4218-952F-87509F8F2116")
-struct DECLSPEC_NOVTABLE IDerivedInterface2 : public IBaseInterface2 {
+struct DECLSPEC_NOVTABLE IDerivedInterface2 : public IBaseInterface2
+{
   virtual void DerivedMethod() = 0;
 };
 
 MSO_CLASS_GUID(MyClass2, "06A68045-D24C-4816-84DC-C18888EC46A9")
-class MyClass2 {
+class MyClass2
+{
   virtual void DerivedMethod() = 0;
 };
 
 MSO_STRUCT_GUID(AlreadyDefinedType21, "6B2050DF-6C60-48ED-809D-A88B2476E5A2")
-struct AlreadyDefinedType21 {
+struct AlreadyDefinedType21
+{
   virtual void Method() = 0;
 };
 
 MSO_CLASS_GUID(AlreadyDefinedType22, "78CEA0B9-2AC5-4FD0-8EE8-C02836BE800D")
-class AlreadyDefinedType22 {
+class AlreadyDefinedType22
+{
   virtual void Method() = 0;
 };
 
-class TypeWithoutGuid2 {};
+class TypeWithoutGuid2
+{
+};
 
-} // namespace MyDetails
-} // namespace MyAppTest
+}} // namespace MyAppTest::MyDetails
 
 // Test that we can pass __uuidof() as a template parameter
-template <const IID &clsid>
-struct MyGuidHolder {
-  const IID &GetGuid() const noexcept {
+template <const IID& clsid>
+struct MyGuidHolder
+{
+  const IID& GetGuid() const noexcept
+  {
     UNREFERENCED_PARAMETER(this);
     return clsid;
   }
@@ -76,18 +90,16 @@ struct MyGuidHolder {
 
 MSO_DEFINE_GUID_TOKEN(MyProductId, "67A42775-79F7-469D-8A28-7348ED9F8D71")
 
-namespace MyAppTest {
-namespace MyDetails {
+namespace MyAppTest { namespace MyDetails {
 
 MSO_DEFINE_GUID_TOKEN(MyProductId2, "AABFD353-B463-41A2-B04C-9A6AB7541D20")
 
-}
-} // namespace MyAppTest
+}} // namespace MyAppTest::MyDetails
 
 MSO_DEFINE_GUID_TOKEN(FailingToParseId, "A39D5FC8-0641-4EEE-8C97-DDEF114D487D")
 
 TestClassComponent(GuidTest, Mso.Guid)
-    TEST_CLASS (GuidTest){static std::string GuidToString(const GUID &guid){char str[37];
+    TEST_CLASS (GuidTest){static std::string GuidToString(const GUID& guid){char str[37];
 sprintf_s(
     str,
     _countof(str),
@@ -106,7 +118,8 @@ sprintf_s(
 return str;
 }
 
-TEST_METHOD(TestTypeGuids) {
+TEST_METHOD(TestTypeGuids)
+{
   TestAssert::AreEqual(std::string("38E23DC7-92B1-4B21-B5FE-6EA786817915"), GuidToString(__uuidof(IBaseInterface)));
   TestAssert::AreEqual(std::string("9E11BC42-1416-45B1-9712-AA4AAD87F78B"), GuidToString(__uuidof(IDerivedInterface)));
   TestAssert::AreEqual(std::string("866E3389-0194-4416-94CE-6BE8A5185387"), GuidToString(__uuidof(MyClass)));
@@ -116,7 +129,8 @@ TEST_METHOD(TestTypeGuids) {
       std::string("8C76ECFE-0715-4DA1-9746-2FD4DE39118F"), GuidToString(__uuidof(AlreadyDefinedType2)));
 }
 
-TEST_METHOD(TestTypeGuids_InNamespace) {
+TEST_METHOD(TestTypeGuids_InNamespace)
+{
   TestAssert::AreEqual(
       std::string("86DE7178-00E6-4A40-8B2C-BA2FBFFB4D06"),
       GuidToString(__uuidof(MyAppTest::MyDetails::IBaseInterface2)));
@@ -133,14 +147,16 @@ TEST_METHOD(TestTypeGuids_InNamespace) {
       GuidToString(__uuidof(MyAppTest::MyDetails::AlreadyDefinedType22)));
 }
 
-TEST_METHOD(TestTypeGuids_uuidof_AsTemplateParameter) {
+TEST_METHOD(TestTypeGuids_uuidof_AsTemplateParameter)
+{
   auto h1 = MyGuidHolder<__uuidof(IBaseInterface)>();
   TestAssert::AreEqual(std::string("38E23DC7-92B1-4B21-B5FE-6EA786817915"), GuidToString(h1.GetGuid()));
   auto h2 = MyGuidHolder<__uuidof(MyAppTest::MyDetails::IBaseInterface2)>();
   TestAssert::AreEqual(std::string("86DE7178-00E6-4A40-8B2C-BA2FBFFB4D06"), GuidToString(h2.GetGuid()));
 }
 
-TEST_METHOD(TestTypeGuids_TypeHasGuid) {
+TEST_METHOD(TestTypeGuids_TypeHasGuid)
+{
   TestAssert::IsTrue(Mso::TypeHasGuid<IBaseInterface>::Value);
   TestAssert::IsTrue(Mso::TypeHasGuid<IDerivedInterface>::Value);
   TestAssert::IsTrue(Mso::TypeHasGuid<MyClass>::Value);
@@ -156,7 +172,8 @@ TEST_METHOD(TestTypeGuids_TypeHasGuid) {
   TestAssert::IsFalse(Mso::TypeHasGuid<MyAppTest::MyDetails::TypeWithoutGuid2>::Value);
 }
 
-TEST_METHOD(TestTypeGuids_DefineGuidToken) {
+TEST_METHOD(TestTypeGuids_DefineGuidToken)
+{
   TestAssert::AreEqual(std::string("67A42775-79F7-469D-8A28-7348ED9F8D71"), GuidToString(__uuidof_token(MyProductId)));
   TestAssert::AreEqual(
       std::string("AABFD353-B463-41A2-B04C-9A6AB7541D20"),
--- libs/motifCpp/tests/motifCppTest.cpp
+++ libs/motifCpp/tests/motifCppTest.cpp
@@ -7,174 +7,217 @@
 // Uncomment to see errors
 //#define MOTIF_TEST_SHOW_ERRORS
 
-TEST_CLASS (MotifCppTest) {
+TEST_CLASS (MotifCppTest)
+{
 #ifdef MOTIF_TEST_SHOW_ERRORS
 
-  TEST_METHOD(GTestFailCalled) {
+  TEST_METHOD(GTestFailCalled)
+  {
     FAIL();
   }
 
-  TEST_METHOD(GTestFailCalledWithMessage) {
+  TEST_METHOD(GTestFailCalledWithMessage)
+  {
     FAIL() << "Test must fail";
   }
 
-  TEST_METHOD(GTestFailCalledWithMessageArgs) {
+  TEST_METHOD(GTestFailCalledWithMessageArgs)
+  {
     FAIL() << TestAssert::FormatMsg("Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(GTestAssertTrueFailed) {
+  TEST_METHOD(GTestAssertTrueFailed)
+  {
     ASSERT_TRUE(std::string("Foo") == "Bar");
   }
 
-  TEST_METHOD(GTestAssertTrueFailedWithMessage) {
+  TEST_METHOD(GTestAssertTrueFailedWithMessage)
+  {
     ASSERT_TRUE(std::string("Foo") == "Bar") << "Test must fail";
   }
 
-  TEST_METHOD(GTestAssertTrueFailedWithMessageArgs) {
+  TEST_METHOD(GTestAssertTrueFailedWithMessageArgs)
+  {
     ASSERT_TRUE(std::string("Foo") == "Bar") << TestAssert::FormatMsg("Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(GTestAssertEQFailed) {
+  TEST_METHOD(GTestAssertEQFailed)
+  {
     ASSERT_EQ(std::string("Foo"), "Bar");
   }
 
-  TEST_METHOD(GTestAssertEQFailedWithMessage) {
+  TEST_METHOD(GTestAssertEQFailedWithMessage)
+  {
     ASSERT_EQ(std::string("Foo"), "Bar") << "Test must fail";
   }
 
-  TEST_METHOD(GTestAssertEQFailedWithMessageArgs) {
+  TEST_METHOD(GTestAssertEQFailedWithMessageArgs)
+  {
     ASSERT_EQ(std::string("Foo"), "Bar") << TestAssert::FormatMsg("Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(TestCheckFailCalled) {
+  TEST_METHOD(TestCheckFailCalled)
+  {
     TestCheckFail();
   }
 
-  TEST_METHOD(TestCheckFailCalledWithMessage) {
+  TEST_METHOD(TestCheckFailCalledWithMessage)
+  {
     TestCheckFail("Test must fail");
   }
 
-  TEST_METHOD(TestCheckFailCalledWithMessageArgs) {
+  TEST_METHOD(TestCheckFailCalledWithMessageArgs)
+  {
     TestCheckFail("Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(TestCheckFailed) {
+  TEST_METHOD(TestCheckFailed)
+  {
     TestCheck(std::string("Foo") == "Bar");
   }
 
-  TEST_METHOD(TestCheckFailedWithMessage) {
+  TEST_METHOD(TestCheckFailedWithMessage)
+  {
     TestCheck(std::string("Foo") == "Bar", "Test must fail");
   }
 
-  TEST_METHOD(TestCheckFailedWithMessageArgs) {
+  TEST_METHOD(TestCheckFailedWithMessageArgs)
+  {
     TestCheck(std::string("Foo") == "Bar", "Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(TestCheckEqualFailed) {
+  TEST_METHOD(TestCheckEqualFailed)
+  {
     TestCheckEqual(std::string("Foo"), "Bar");
   }
 
-  TEST_METHOD(TestCheckEqualFailedWithMessage) {
+  TEST_METHOD(TestCheckEqualFailedWithMessage)
+  {
     TestCheckEqual(std::string("Foo"), "Bar", "Test must fail");
   }
 
-  TEST_METHOD(TestCheckEqualFailedWithMessageArgs) {
+  TEST_METHOD(TestCheckEqualFailedWithMessageArgs)
+  {
     TestCheckEqual(std::string("Foo"), "Bar", "Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(TestCheckCrashFailed) {
+  TEST_METHOD(TestCheckCrashFailed)
+  {
     TestCheckCrash(VerifyElseCrashSz(true, "No crash"));
   }
 
-  TEST_METHOD(TestCheckCrashFailedWithMessage) {
+  TEST_METHOD(TestCheckCrashFailedWithMessage)
+  {
     TestCheckCrash(VerifyElseCrashSz(true, "No crash"), "Test must fail");
   }
 
-  TEST_METHOD(TestCheckCrashFailedWithMessageArgs) {
+  TEST_METHOD(TestCheckCrashFailedWithMessageArgs)
+  {
     TestCheckCrash(VerifyElseCrashSz(true, "No crash"), "Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(TestCheckCrashSucceeded) {
+  TEST_METHOD(TestCheckCrashSucceeded)
+  {
     TestCheckCrash(VerifyElseCrashSz(false, "Must crash"));
   }
 
-  TEST_METHOD(TestCheckCrashSucceededWithMessage) {
+  TEST_METHOD(TestCheckCrashSucceededWithMessage)
+  {
     TestCheckCrash(VerifyElseCrashSz(false, "Must crash"), "Test must succeed");
   }
 
-  TEST_METHOD(TestCheckCrashSucceededWithMessageArgs) {
+  TEST_METHOD(TestCheckCrashSucceededWithMessageArgs)
+  {
     TestCheckCrash(VerifyElseCrashSz(false, "Must crash"), "Test %s %s %d", "must", "succeed", 2);
   }
 
-  TEST_METHOD(TestCheckTerminateFailed) {
+  TEST_METHOD(TestCheckTerminateFailed)
+  {
     TestCheckTerminate(2 + 2);
   }
 
-  TEST_METHOD(TestCheckTerminateFailedWithMessage) {
+  TEST_METHOD(TestCheckTerminateFailedWithMessage)
+  {
     TestCheckTerminate(2 + 2, "Test must fail");
   }
 
-  TEST_METHOD(TestCheckTerminateFailedWithMessageArgs) {
+  TEST_METHOD(TestCheckTerminateFailedWithMessageArgs)
+  {
     TestCheckTerminate(2 + 2, "Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(TestCheckTerminateSucceeded) {
+  TEST_METHOD(TestCheckTerminateSucceeded)
+  {
     TestCheckTerminate(std::terminate());
   }
 
-  TEST_METHOD(TestCheckTerminateSucceededWithMessage) {
+  TEST_METHOD(TestCheckTerminateSucceededWithMessage)
+  {
     TestCheckTerminate(std::terminate(), "Test must succeed");
   }
 
-  TEST_METHOD(TestCheckTerminateSucceededWithMessageArgs) {
+  TEST_METHOD(TestCheckTerminateSucceededWithMessageArgs)
+  {
     TestCheckTerminate(std::terminate(), "Test %s %s %d", "must", "succeed", 2);
   }
 
-  TEST_METHOD(TestCheckExceptionFailed) {
+  TEST_METHOD(TestCheckExceptionFailed)
+  {
     TestCheckException(std::exception, 2 + 2);
   }
 
-  TEST_METHOD(TestCheckExceptionFailedWithMessage) {
+  TEST_METHOD(TestCheckExceptionFailedWithMessage)
+  {
     TestCheckException(std::exception, 2 + 2, "Test must fail");
   }
 
-  TEST_METHOD(TestCheckExceptionFailedWithMessageArgs) {
+  TEST_METHOD(TestCheckExceptionFailedWithMessageArgs)
+  {
     TestCheckException(std::exception, 2 + 2, "Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(TestCheckExceptionSucceeded) {
+  TEST_METHOD(TestCheckExceptionSucceeded)
+  {
     TestCheckException(std::exception, throw std::exception());
   }
 
-  TEST_METHOD(TestCheckExceptionSucceededWithMessage) {
+  TEST_METHOD(TestCheckExceptionSucceededWithMessage)
+  {
     TestCheckException(std::exception, throw std::exception(), "Test must succeed");
   }
 
-  TEST_METHOD(TestCheckExceptionSucceededWithMessageArgs) {
+  TEST_METHOD(TestCheckExceptionSucceededWithMessageArgs)
+  {
     TestCheckException(std::exception, throw std::exception(), "Test %s %s %d", "must", "succeed", 2);
   }
 
-  TEST_METHOD(TestCheckNoThrowFailed) {
+  TEST_METHOD(TestCheckNoThrowFailed)
+  {
     TestCheckNoThrow(throw std::exception());
   }
 
-  TEST_METHOD(TestCheckNoThrowFailedWithMessage) {
+  TEST_METHOD(TestCheckNoThrowFailedWithMessage)
+  {
     TestCheckNoThrow(throw std::exception(), "Test must fail");
   }
 
-  TEST_METHOD(TestCheckNoThrowFailedWithMessageArgs) {
+  TEST_METHOD(TestCheckNoThrowFailedWithMessageArgs)
+  {
     TestCheckNoThrow(throw std::exception(), "Test %s %s %d", "must", "fail", 2);
   }
 
-  TEST_METHOD(TestCheckNoThrowSucceeded) {
+  TEST_METHOD(TestCheckNoThrowSucceeded)
+  {
     TestCheckNoThrow(2 + 2);
   }
 
-  TEST_METHOD(TestCheckNoThrowSucceededWithMessage) {
+  TEST_METHOD(TestCheckNoThrowSucceededWithMessage)
+  {
     TestCheckNoThrow(2 + 2, "Test must succeed");
   }
 
-  TEST_METHOD(TestCheckNoThrowSucceededWithMessageArgs) {
+  TEST_METHOD(TestCheckNoThrowSucceededWithMessageArgs)
+  {
     TestCheckNoThrow(2 + 2, "Test %s %s %d", "must", "succeed", 2);
   }
 #endif
